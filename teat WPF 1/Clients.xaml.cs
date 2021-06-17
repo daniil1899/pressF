a0d2c03009b7f76d29f7fabf5e1fc26bc04d3101
Даниил Волнухин <dreamer1899@outlook.com>
2021-06-17 12:23:32 +0300

    session1

---
+++ teat WPF 1/Clients.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace teat_WPF_1
+{
+    /// <summary>
+    /// Логика взаимодействия для Clients.xaml
+    /// </summary>
+    public partial class Clients : Window
+    {
+        public Clients()
+        {
+            CC.ItemsSource = new sessionsEntities().apartments.ToList();
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Rieltor rieltor = new Rieltor();
+            rieltor.Show();
+            Hide();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ teat WPF 1/Rieltor.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace teat_WPF_1
+{
+    /// <summary>
+    /// Логика взаимодействия для Rieltor.xaml
+    /// </summary>
+    public partial class Rieltor : Window
+        
+        
+    {
+
+        public Rieltor()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Clients clients = new Clients();
+            clients.Show();
+            Hide();
+        }
+
+        private void RR_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+          
+        }
+    }
+}
